4775d440ac8323133c762f8d924205526babc353
yifan <yifan_chen@himirror.com>
2021-03-30 15:14:37 +0800

    init for linode branch

--- src/main.js
+++ src/main.js
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueMaterial from 'vue-material'
-import ElementUI from 'element-ui';
-
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
-Vue.use(ElementUI)
 
 
 new Vue({
   router,
-  el: '#app',
   render: h => h(App),
 }).$mount('#app')
